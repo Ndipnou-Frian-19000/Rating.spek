4ac4ddaf532145b2e2833ce18acaf3a0750dc17c
Ndipnou-frain-19000 <ndipnouclaude@gmail.com>
2024-12-25 04:10:28 +0000

    added JS

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// Add functionality for the helpful buttons
+document.querySelectorAll('.helpful-buttons button').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Thank you for your feedback!');
+    });
+  });
+  
